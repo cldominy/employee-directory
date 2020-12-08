d9a748aa597f8c5d55045ace0c069ba3fa527c90
Chris Dominy <cdominy@utexas.edu>
2020-12-07 21:04:20 -0600

    Removed router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home"
 
 
 function App() {
-    return (
-        <Router>
-            <Route exact path="/" component={Home}/>
-        </Router>
-    );
+    return <Home></Home>;
 }
 
 export default App;
